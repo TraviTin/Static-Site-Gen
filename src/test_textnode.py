37642feea6ba90366c311beffe5ee1d7a2a7cb4e
TraviTin <scoutballoons@gmail.com>
2026-06-18 19:09:37 -0500

    added enum types to trasnfer text into html nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import HtmlNode
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -38,6 +38,36 @@ class TestTextNode(unittest.TestCase):
         node = HtmlNode("<p>","hello", [], {})
         node2 = HtmlNode("<p>","hello", [], {})
         self.assertEqual(node.tag, node2.tag)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_bold(self):
+        node = TextNode("This is a BOLD TEXT node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a BOLD TEXT node")
+
+    def test_text_link(self):
+        node = TextNode("google", TextType.LINK, "google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "google")
+        self.assertEqual(html_node.props, {"href":"google.com"})
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
 
 
 if __name__ == "__main__":
