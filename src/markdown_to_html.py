2f239a2d2d0822ebd9e3f5262712a08460b5977e
TraviTin <scoutballoons@gmail.com>
2026-06-23 22:23:12 -0500

    finished markdown to html functions

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -3,34 +3,41 @@ from textnode import *
 from inline_markdown import *
 from htmlnode import *
 
-def markdown_to_html_node(markdown: str) -> HtmlNode:
+
+def markdown_to_html_node(markdown: str) -> ParentNode:
     blocks = markdown_to_blocks(markdown)
     children_list = []
     for block in blocks:
-        determine_type = block_to_block_type(block)
-        match determine_type:
-            case BlockType.PARAGRAPH:
-                html_block = paragraph_node(block)
-            case BlockType.HEADING:
-                html_block = heading_node(block)
-            case BlockType.CODE:
-                html_block = code_node(block)
-            case BlockType.QUOTE:
-                html_block = quote_node(block)
-            case BlockType.UNORDERED_LIST:
-                html_block = unordered_list_node(block)
-            case BlockType.ORDERED_LIST:
-                html_block = ordered_list_node(block)
+        html_block = block_to_html_node(block)
         children_list.append(html_block)
-    div = ParentNode(tag = "div", children = children_list)
+    div = ParentNode(tag="div", children=children_list)
     return div
 
+def block_to_html_node(block: str) -> ParentNode:
+    determine_type = block_to_block_type(block)
+    match determine_type:
+        case BlockType.PARAGRAPH:
+            html_block = paragraph_node(block)
+        case BlockType.HEADING:
+            html_block = heading_node(block)
+        case BlockType.CODE:
+            html_block = code_node(block)
+        case BlockType.QUOTE:
+            html_block = quote_node(block)
+        case BlockType.UNORDERED_LIST:
+            html_block = unordered_list_node(block)
+        case BlockType.ORDERED_LIST:
+            html_block = ordered_list_node(block)
+        case _:
+            raise ValueError("invalid block type")
+    return html_block
 
 
 def paragraph_node(block: str) -> ParentNode:
-    stripped = block.replace("\n", " ")
-    nodes = text_to_children(stripped)
-    return ParentNode(tag = "p", children = nodes)
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    nodes = text_to_children(paragraph)
+    return ParentNode(tag="p", children=nodes)
 
 
 def heading_node(block: str) -> ParentNode:
@@ -40,61 +47,58 @@ def heading_node(block: str) -> ParentNode:
             level += 1
         else:
             break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
     text_block = block[level + 1:]
     nodes = text_to_children(text_block)
-    return ParentNode(tag = f"h{level}", children = nodes)
+    return ParentNode(tag=f"h{level}", children=nodes)
 
 
 def code_node(block: str) -> ParentNode:
-    line = block.split("\n")
-    inner_lines = line[1:-1]
-    stripped = "\n".join(inner_lines)
-    stripped = stripped + "\n"
-    stripped_node = TextNode(text = stripped, text_type=TextType.TEXT)
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    stripped = block[4:-3]
+    stripped_node = TextNode(text=stripped, text_type=TextType.TEXT)
     stripped_html_node = text_node_to_html_node(stripped_node)
-    code = ParentNode(tag = "code", children = [stripped_html_node])
-    pre = ParentNode(tag = "pre", children = [code])
-    return pre
+    code = ParentNode(tag="code", children=[stripped_html_node])
+    return ParentNode(tag="pre", children=[code])
+
 
 def quote_node(block: str) -> ParentNode:
     split_by_lines = block.split("\n")
     cleaned_lines = []
     for line in split_by_lines:
         if line.startswith(">"):
-            cleaned = line[1:]
-            cleaned = cleaned.strip()
-            cleaned_lines.append(cleaned)
+            cleaned_lines.append(line.lstrip(">").strip())
     cleaned_lines = " ".join(cleaned_lines)
     node = text_to_children(cleaned_lines)
-    return ParentNode(tag = "blockquote", children = node)
-
+    return ParentNode(tag="blockquote", children=node)
 
 
 def unordered_list_node(block: str) -> HtmlNode:
     split_by_lines = block.split("\n")
     list_of_all_nodes = []
     for lines in split_by_lines:
-        if lines.startswith("-"):
-            stripped = lines[1:]
-            stripped = stripped.strip()
-            list_nodes = text_to_children(stripped)
-            node = ParentNode(tag = "li", children = list_nodes)
-            list_of_all_nodes.append(node)
-    return ParentNode(tag = "ul", children = list_of_all_nodes)
-        
-
-
-
+        if not lines.startswith(">"):
+            raise ValueError("invalid quote block")
+        stripped = lines[2:]
+        list_nodes = text_to_children(stripped)
+        node = ParentNode(tag="li", children=list_nodes)
+        list_of_all_nodes.append(node)
+    return ParentNode(tag="ul", children=list_of_all_nodes)
 
 
 def ordered_list_node(block: str) -> HtmlNode:
     split_by_lines = block.split("\n")
-    final_block = ""
-    for num, line in enumerate(split_by_lines, start=1):
-        replaced = line.replace(f"{num}. ", "<li>")
-        final_block += f"{replaced}</li>"
-    final_block = f"<ol>{final_block}</ol>"
-    return HtmlNode(tag = "ol", value = final_block)
+    list_of_final_nodes = []
+    for lines in split_by_lines:
+        separate_lines = lines.split(". ", 1)
+        line = separate_lines[1]
+        stripped_line = line.strip()
+        list_nodes = text_to_children(stripped_line)
+        node = ParentNode(tag="li", children=list_nodes)
+        list_of_final_nodes.append(node)
+    return ParentNode(tag="ol", children=list_of_final_nodes)
 
 
 def text_to_children(block: str) -> list[HtmlNode]:
@@ -104,4 +108,3 @@ def text_to_children(block: str) -> list[HtmlNode]:
         nodes = text_node_to_html_node(node)
         html_children.append(nodes)
     return html_children
-
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 from block_markdown import *
-
+from markdown_to_html import *
 
 class TestTextNode(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -88,6 +88,51 @@ This is the same paragraph on a new line
         self.assertEqual(
             BlockType.PARAGRAPH,
             answer,
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+    "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+    )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_ordered_list(self):
+        md = """
+1. first
+2. second
+3. third
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>first</li><li>second</li><li>third</li></ol></div>",
         )
 
 if __name__ == "__main__":
